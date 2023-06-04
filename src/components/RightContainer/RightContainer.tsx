af3d73ff58daf1916240eab1acaf1c8d8e69526b
wolfgunblood <jalajdorai1@gmail.com>
2023-06-04 13:36:16 +0530

    Styled Right Nav Bar

--- src/components/RightContainer/RightContainer.tsx
+++ src/components/RightContainer/RightContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './RightContainer.scss';
+import { MdOutlineCandlestickChart } from "react-icons/md";
 
 const RightContainer = () => {
   return (
     <div className='right-container'>
         <nav className='right-nav'>
+            <MdOutlineCandlestickChart size={30} />
             <ul className='right-nav-list'>
                 <li className=' nav-items active'>
                     <p>Dashboard</p>
