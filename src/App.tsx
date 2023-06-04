69e0b331a4d9068f98aad5b241e36cddfb241714
wolfgunblood <jalajdorai1@gmail.com>
2023-06-04 13:02:01 +0530

    Initaized Left and Right Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import LeftContainer from './components/LeftContainer/LeftContainer';
+import RightContainer from './components/RightContainer/RightContainer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <LeftContainer />
+      <RightContainer />
+    </div>
   )
 }
 
---
+++ src/components/LeftContainer/LeftContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftContainer = () => {
+  return (
+    <div>LeftContainer</div>
+  )
+}
+
+export default LeftContainer
